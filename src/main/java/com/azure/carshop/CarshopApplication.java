6f71742670b9dbdada6b803d7052478729d8c6df
Riteshchoubey <riteshchaubey78@gmail.com>
2023-09-11 23:51:46 -0700

    redis changes applied

--- src/main/java/com/azure/carshop/CarshopApplication.java
+++ src/main/java/com/azure/carshop/CarshopApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cache.annotation.EnableCaching;
 
-import redis.clients.jedis.Jedis;
+
+
+
 
 
 @SpringBootApplication
@@ -13,36 +15,13 @@ import redis.clients.jedis.Jedis;
 public class CarshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarshopApplication.class, args);
-		testAzureRedisConnection();
+		SpringApplication.run(CarshopApplication.class, args);	
 	}
-	public static void testAzureRedisConnection() {
-        String host = "product-redis.redis.cache.windows.net";
-        int port = 6380; // Azure Redis Cache port
-        String password = "paxoaixlDJx5zYc9Nzt0WTO2IRBM4hnt5AzCaJlsOaM=";
-
-        Jedis jedis = null;
-
-        try {
-            // Create a Jedis connection to Azure Redis Cache
-            jedis = new Jedis(host, port);
-            jedis.auth(password);
-
-            // Test the connection by performing a simple operation
-            String pingResponse = jedis.ping();
-
-            if ("PONG".equalsIgnoreCase(pingResponse)) {
-                System.out.println("Connected to Azure Redis Cache successfully.");
-            } else {
-                System.err.println("Failed to connect to Azure Redis Cache.");
-            }
-        } catch (Exception e) {
-            System.err.println("Failed to connect to Azure Redis Cache. Error: " + e.getMessage());
-        } finally {
-            if (jedis != null) {
-                jedis.close();
-            }
-        }
+	
     }
 
-}
+
+
+	
+
+
